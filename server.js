4452a17a2351b911e01b5acdc3fea015230d97d1
tuthogi123 <evankarani076@gmail.com>
2023-11-30 09:38:36 +0300

    my comit

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const multer = require('multer');
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/campx', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://Tuthogi:<password>@cluster0.swcumaa.mongodb.net/campx', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// Define MongoDB schema and model
+
 const partySchema = new mongoose.Schema({
   venue: String,
   date: String,
@@ -17,25 +19,23 @@ const partySchema = new mongoose.Schema({
 
 const Party = mongoose.model('Party', partySchema);
 
-// Configure Multer for handling file uploads
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/campx.html');
 });
 
-// Handle file upload and store data in the database
 app.post('/upload', upload.fields([]), async (req, res) => {
   const { section, partyVenue, partyDate, partyTime, bootcampVenue, bootcampDate, bootcampTime, bootcampDescription } = req.body;
 
   try {
-    // Save data to MongoDB based on the selected section
+  
     if (section === 'partys') {
       await Party.create({ venue: partyVenue, date: partyDate, time: partyTime });
     } else if (section === 'bootcamp') {
-
+     
     }
 
     res.status(200).send('File uploaded and data stored successfully!');
